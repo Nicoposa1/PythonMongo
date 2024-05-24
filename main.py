86150b12050825cb8887a37f0ab25806877e7584
Nicolas Posa <nicoposa57@gmail.com>
2024-05-23 21:33:37 -0300

    connected to mongo database

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+from urllib.parse import quote_plus
 
 app = FastAPI()
 
+uri = "mongodb+srv://<username>:<password>@cluster0.zaxvna2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+escaped_uri = uri.replace("<username>", quote_plus("nicoposa57@gmail.com")).replace("<password>", quote_plus("BZ8XzVoMnNVeUz84"))
+
+@app.on_event("startup")
+def startup_db_client():
+    app.mongodb_client = MongoClient(escaped_uri)
+    print("Connected to the MongoDB database!")
+
+@app.on_event("shutdown")
+def shutdown_db_client():
+    app.mongodb_client.close()
+
 @app.get("/")
 async def root():
-  return {"Message" : "Hello world!"}
+    return {"Message" : "Hello world!"}
