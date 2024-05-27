b7c69c61019eed683e6bd7c1fc0b752044dd46af
Nicolas Posa <nicoposa57@gmail.com>
2024-05-27 01:53:09 -0300

    project finished

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from urllib.parse import quote_plus
+from dotenv import load_dotenv
+import os
+
+# Cargar variables de entorno desde el archivo .env
+load_dotenv()
 
 app = FastAPI()
 
-uri = "mongodb+srv://<username>:<password>@cluster0.zaxvna2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-escaped_uri = uri.replace("<username>", quote_plus("nicoposa57@gmail.com")).replace("<password>", quote_plus("BZ8XzVoMnNVeUz84"))
+# Obtener las variables de entorno
+username = os.getenv("MONGO_USERNAME")
+password = os.getenv("MONGO_PASSWORD")
+raw_uri = os.getenv("ATLAS_URI")
+
+if not username or not password or not raw_uri:
+    raise ValueError("No ATLAS_URI, MONGO_USERNAME or MONGO_PASSWORD found in environment variables")
+
+# Escapar el usuario y la contraseña
+escaped_username = quote_plus(username)
+escaped_password = quote_plus(password)
+
+# Reemplazar en la URI
+escaped_uri = raw_uri.replace("<username>", escaped_username).replace("<password>", escaped_password)
 
 @app.on_event("startup")
 def startup_db_client():
     app.mongodb_client = MongoClient(escaped_uri)
+    app.database = app.mongodb_client[os.getenv("DB_NAME")]  # Asigna la base de datos a app.database
     print("Connected to the MongoDB database!")
 
 @app.on_event("shutdown")
@@ -18,4 +36,8 @@ def shutdown_db_client():
 
 @app.get("/")
 async def root():
-    return {"Message" : "Hello world!"}
+    return {"Message": "Hello world!"}
+
+# Incluye tu router con un prefijo para la API
+from routes import router as book_router
+app.include_router(book_router, prefix="/api/v1")
--- routes.py
+++ routes.py
@@ -5,3 +5,60 @@ from typing import List
 from models import Book, BookUpdate
 
 router = APIRouter()
+
+
+@router.post("/", response_description="Create a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
+def create_book(request: Request, book: Book = Body(...)):
+    book = jsonable_encoder(book)
+    new_book = request.app.database["books"].insert_one(book)
+    created_book = request.app.database["books"].find_one(
+        {"_id": new_book.inserted_id}
+    )
+
+    return created_book
+
+@router.get("/", response_description="List all books", response_model=List[Book])
+def list_books(request: Request):
+    books = request.app.database["books"].find()
+    return books
+
+
+@router.get("/{id}", response_description="Get a single book by id", response_model=Book)
+def find_book(id: str, request: Request):
+    if (book := request.app.database["books"].find_one({"_id": id})) is not None:
+        return book
+    raise HTTPException(
+        status_code=404, detail=f"Book with id {id} not found")
+
+
+@router.put("/{id}", response_description="Update a book", response_model=Book)
+def update_book(id: str, request: Request, book: BookUpdate = Body(...)):
+    book = {k: v for k, v in book.dict().items() if v is not None}
+    if (len(book) >= 1):
+        update_result = request.app.database["books"].update_one(
+            {"_id": id}, {"$set": book}
+        )
+        if update_result.modified_count == 1:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Book with id {id} not found")
+
+    if (
+        existing_book := request.app.database["books"].find_one({"_id": id})
+    ) is not None:
+        return existing_book
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Book with id {id} not found")
+
+
+@router.delete("/{id}", response_description="Book with id {id} has been deleted")
+def delete_book(id: str, request: Request):
+    delete_result = request.app.database["books"].delete_one({"_id": id})
+
+    if delete_result.deleted_count == 1:
+        return {"status": f"Book with id {id} has been deleted successfully"}
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail=f"Book with id {id} not found")
+
+
